e60b0f57e9afcd63e25ba6be9a150abe32e067c7
AbhishekEV <abhishek.ev.ext@siemens-healthineers.com>
2025-05-10 02:08:33 +0530

    update 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,11 @@ import { NavbarComponent } from '../common/navbar/navbar.component';
 import { ProductsComponent } from '../products/products/products.component';
 import { FooterComponent } from '../common/footer/footer.component';
 import { GalleryComponent } from '../gallery/gallery.component';
+import { AboutComponent } from '../about/about.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProductsComponent , FooterComponent,GalleryComponent,NavbarComponent],
+  imports: [RouterOutlet, ProductsComponent , FooterComponent,GalleryComponent,NavbarComponent,AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/products/products/products.component.ts
+++ src/products/products/products.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from '../../common/navbar/navbar.component';
 import { FooterComponent } from '../../common/footer/footer.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCircleChevronRight } from '@fortawesome/free-solid-svg-icons';
 
 
 @Component({
@@ -12,26 +13,27 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
   styleUrl: './products.component.css'
 })
 export class ProductsComponent {
+  faCircleChevronRight = faCircleChevronRight;
   faloodaCategories = [
     {
       title: 'The Great Indian Falooda',
       subtitle: 'Chandigarh Ki Gulab (Kulfi Falooda)',
       items: ['Shalimar Falooda', 'Pyaare Gulab Jamun', 'Mumbai Ka King', 'Kesar Pista Falooda', 'Mango Kulfi Falooda'],
-      imageUrl: 'https://img.freepik.com/free-photo/strawberry-milkshake-with-ice-table_140725-5486.jpg?ga=GA1.1.104214007.1745296814&semt=ais_hybrid&w=740',
+      imageUrl: 'https://img.freepik.com/free-photo/lychee-jelly-seasonal-fruit-beautifully-decorated-thai-dessert-concept_1150-23501.jpg?ga=GA1.1.104214007.1745296814&semt=ais_hybrid&w=740',
       varieties: 18,
     },
     {
       title: 'The League of Falooda Nation',
       subtitle: 'Chandigarh Ki Gulab (Kulfi Falooda)',
       items: ['Shalimar Falooda', 'Pyaare Gulab Jamun', 'Mumbai Ka King', 'Kesar Pista Falooda', 'Mango Kulfi Falooda'],
-      imageUrl: 'https://img.freepik.com/free-photo/four-different-cocktail-smoothies-with-milky-cream-top_114579-1097.jpg?ga=GA1.1.104214007.1745296814&semt=ais_hybrid&w=740',
+      imageUrl: 'https://img.freepik.com/free-photo/lychee-jelly-seasonal-fruit-beautifully-decorated-thai-dessert-concept_1150-23501.jpg?ga=GA1.1.104214007.1745296814&semt=ais_hybrid&w=740',
       varieties: 12,
     },
     {
       title: 'The Great Indian Falooda',
       subtitle: 'Chandigarh Ki Gulab (Kulfi Falooda)',
       items: ['Shalimar Falooda', 'Pyaare Gulab Jamun', 'Mumbai Ka King', 'Kesar Pista Falooda', 'Mango Kulfi Falooda'],
-      imageUrl: 'https://img.freepik.com/premium-photo/mixed-fruit-ice-cream-falooda_527904-483.jpg?ga=GA1.1.104214007.1745296814&semt=ais_hybrid&w=740',
+      imageUrl: 'https://img.freepik.com/free-photo/lychee-jelly-seasonal-fruit-beautifully-decorated-thai-dessert-concept_1150-23501.jpg?ga=GA1.1.104214007.1745296814&semt=ais_hybrid&w=740',
       varieties: 18,
     },
     {
